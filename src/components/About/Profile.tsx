db17e85881052dbe6b2e7f6c9d012e19769ef929
ConorCrutchley <conorc1993@hotmail.co.uk>
2025-08-31 16:10:43 +0100

    feat: Added profile picture to about section

--- src/components/About/Profile.tsx
+++ src/components/About/Profile.tsx
@@ -1,5 +1,15 @@
+import profileImg from '@/assets/conor-crutchley-in-salmon-blazer-and-pink-shirt-with-green-gradient.png';
+import styles from '@/styles/profile.module.css';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className={styles.profile}>
+      <img
+        src={profileImg}
+        alt="conor crutchley in salmon blazer and pink shirt with green gradient"
+      />
+    </div>
+  );
 };
 
 export default Profile;
