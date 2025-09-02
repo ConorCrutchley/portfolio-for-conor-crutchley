9b8c10c0776cef50dccfcc4083d2253b4ffd8609
ConorCrutchley <conorc1993@hotmail.co.uk>
2025-09-02 18:35:09 +0100

    refactor: Updated the speed of various lines in the background

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -12,7 +12,7 @@ const Background = () => {
       <defs>
         <pattern
           id="verticalOpaqueLines"
-          className={styles.verticalOpauePattern}
+          className={`${styles.verticalPattern} ${styles.verticalOpauePattern}`}
           width="800"
           height="800"
           patternUnits="userSpaceOnUse"
@@ -160,9 +160,9 @@ const Background = () => {
         </pattern>
         <pattern
           id="horizontalOpaqueLines"
-          className={styles.horizontalOpaquePattern}
-          width="400"
-          height="400"
+          className={`${styles.horizontalPattern} ${styles.horizontalOpaquePattern}`}
+          width="800"
+          height="800"
           patternUnits="userSpaceOnUse"
         >
           <g className={styles.horizontalGroup}>
@@ -217,24 +217,24 @@ const Background = () => {
           </g>
         </pattern>
         <pattern
-          id="horizontalLines"
-          className={styles.horizontalPattern}
-          width="400"
-          height="400"
+          id="horizontalSecondaryLines"
+          className={`${styles.horizontalPattern} ${styles.horizontalSecondaryPattern}`}
+          width="800"
+          height="800"
           patternUnits="userSpaceOnUse"
         >
           <g className={styles.horizontalGroup}>
-            <rect width={48} height={6} x={154} y={311} opacity={0.66} rx={3} />
+            <rect width={39} height={6} x={20.5} y={174} opacity={0.7} rx={3} />
+            <rect width={42} height={6} x={40} y={440} opacity={0.57} rx={3} />
             <rect
-              width={57}
+              width={63}
               height={6}
-              x={451.5}
-              y={380}
-              opacity={0.54}
+              x={169.5}
+              y={541}
+              opacity={0.97}
               rx={3}
             />
-            <rect width={52} height={6} x={26} y={340} opacity={0.95} rx={3} />
-            <rect width={39} height={6} x={20.5} y={174} opacity={0.7} rx={3} />
+            <rect width={48} height={6} x={154} y={311} opacity={0.66} rx={3} />
             <rect
               width={65}
               height={6}
@@ -243,24 +243,17 @@ const Background = () => {
               opacity={0.61}
               rx={3}
             />
+            <rect width={46} height={6} x={242} y={96} opacity={0.54} rx={3} />
             <rect
-              width={71}
+              width={55}
               height={6}
-              x={692.5}
-              y={693}
-              opacity={0.41}
+              x={291.5}
+              y={480}
+              opacity={0.4}
               rx={3}
             />
-            <rect width={54} height={6} x={718} y={157} rx={3} />
-            <rect
-              width={47}
-              height={6}
-              x={650.5}
-              y={518}
-              opacity={0.75}
-              rx={3}
-            />
-
+            <rect width={64} height={6} x={326} y={366} opacity={0.51} rx={3} />
+            <rect width={42} height={6} x={343} y={661} opacity={0.57} rx={3} />
             <rect
               width={57}
               height={6}
@@ -269,7 +262,7 @@ const Background = () => {
               opacity={0.52}
               rx={3}
             />
-            <rect width={52} height={6} x={135} y={143} opacity={0.77} rx={3} />
+            <rect width={38} height={6} x={551} y={612} opacity={0.6} rx={3} />
             <rect
               width={55}
               height={6}
@@ -279,11 +272,52 @@ const Background = () => {
               rx={3}
             />
             <rect
+              width={49}
+              height={6}
+              x={570.5}
+              y={747}
+              opacity={0.59}
+              rx={3}
+            />
+            <rect width={44} height={6} x={489} y={677} opacity={0.92} rx={3} />
+            <rect
+              width={71}
+              height={6}
+              x={692.5}
+              y={693}
+              opacity={0.41}
+              rx={3}
+            />
+            <rect
               width={45}
               height={6}
               x={705.5}
               y={42}
               opacity={0.59}
+              rx={3}
+            />
+            <rect width={40} height={6} x={739} y={486} opacity={0.97} rx={3} />
+          </g>
+        </pattern>
+        <pattern
+          id="horizontalLines"
+          className={styles.horizontalPattern}
+          width="800"
+          height="800"
+          patternUnits="userSpaceOnUse"
+        >
+          <g className={styles.horizontalGroup}>
+            <rect width={52} height={6} x={26} y={340} opacity={0.95} rx={3} />
+            <rect width={56} height={6} x={47} y={539} opacity={0.78} rx={3} />
+            <rect width={52} height={6} x={135} y={143} opacity={0.77} rx={3} />
+            <rect width={50} height={6} x={74} y={239} opacity={0.96} rx={3} />
+            <rect width={72} height={6} x={180} y={724} opacity={0.82} rx={3} />
+            <rect
+              width={57}
+              height={6}
+              x={451.5}
+              y={380}
+              opacity={0.54}
               rx={3}
             />
             <rect
@@ -295,42 +329,19 @@ const Background = () => {
               rx={3}
             />
             <rect
-              width={55}
+              width={33}
               height={6}
-              x={291.5}
-              y={480}
-              opacity={0.4}
+              x={183.5}
+              y={30}
+              opacity={0.69}
               rx={3}
             />
-            <rect width={46} height={6} x={242} y={96} opacity={0.54} rx={3} />
             <rect
               width={49}
               height={6}
               x={471.5}
               y={158}
               opacity={0.78}
-              rx={3}
-            />
-            <rect width={40} height={6} x={739} y={486} opacity={0.97} rx={3} />
-            <rect width={56} height={6} x={47} y={539} opacity={0.78} rx={3} />
-            <rect width={64} height={6} x={326} y={366} opacity={0.51} rx={3} />
-            <rect width={42} height={6} x={343} y={661} opacity={0.57} rx={3} />
-            <rect
-              width={49}
-              height={6}
-              x={570.5}
-              y={747}
-              opacity={0.59}
-              rx={3}
-            />
-            <rect width={38} height={6} x={551} y={612} opacity={0.6} rx={3} />
-            <rect width={44} height={6} x={489} y={677} opacity={0.92} rx={3} />
-            <rect
-              width={33}
-              height={6}
-              x={183.5}
-              y={30}
-              opacity={0.69}
               rx={3}
             />
             <rect
@@ -341,17 +352,15 @@ const Background = () => {
               opacity={0.37}
               rx={3}
             />
-            <rect width={42} height={6} x={40} y={440} opacity={0.57} rx={3} />
             <rect
-              width={63}
+              width={47}
               height={6}
-              x={169.5}
-              y={541}
-              opacity={0.97}
+              x={650.5}
+              y={518}
+              opacity={0.75}
               rx={3}
             />
-            <rect width={72} height={6} x={180} y={724} opacity={0.82} rx={3} />
-            <rect width={50} height={6} x={74} y={239} opacity={0.96} rx={3} />
+            <rect width={54} height={6} x={718} y={157} rx={3} />
           </g>
         </pattern>
       </defs>
@@ -360,6 +369,7 @@ const Background = () => {
       <rect width="100%" height="100%" fill="url(#verticalOpaqueLines)" />
       <rect width="100%" height="100%" fill="url(#verticalLines)" />
       <rect width="100%" height="100%" fill="url(#horizontalOpaqueLines)" />
+      <rect width="100%" height="100%" fill="url(#horizontalSecondaryLines)" />
       <rect width="100%" height="100%" fill="url(#horizontalLines)" />
     </svg>
   );
