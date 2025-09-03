918eb1c209a12c157e574f5e1e0cbffe7d603fe9
ConorCrutchley <conorc1993@hotmail.co.uk>
2025-09-03 07:06:47 +0100

    feat: Added experience timeline

--- src/components/Experience/ExperienceList.tsx
+++ src/components/Experience/ExperienceList.tsx
@@ -1,13 +1,21 @@
 import Experience from '@/components/Experience/Experience';
 import { experience } from '@/models/experience.model';
+import styles from '@/styles/experience/experience-list.module.css';
 
 const ExperienceList = () => {
   return (
     <section id="experience">
       <h2>Experience</h2>
-      {experience.map((xp) => (
-        <Experience key={`experience_${xp.key}`} experience={xp} />
-      ))}
+      <div className={styles['experience-list']}>
+        <div className={styles['experience-list-items']}>
+          {experience.map((xp) => (
+            <div className={styles['experience-list-item']}>
+              <div className={styles['experience-list-item-bullet']}></div>
+              <Experience key={`experience_${xp.key}`} experience={xp} />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
