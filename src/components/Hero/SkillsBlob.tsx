5c0e0805ed21ff0e833fb76589eedb1b7b2c1d41
ConorCrutchley <conorc1993@hotmail.co.uk>
2025-09-07 09:03:52 +0100

    feat: Set skill logo animations for screens between 600px and 900px

--- src/components/Hero/SkillsBlob.tsx
+++ src/components/Hero/SkillsBlob.tsx
@@ -4,7 +4,7 @@ import styles from '@/styles/hero/skills-blob.module.css';
 const SkillsBlob = () => {
   return (
     <div className={styles['blob-container']}>
-      <div className={styles['blob']}></div>;
+      <div className={styles['blob']}></div>
       <SkillsBlobLogos />
     </div>
   );
