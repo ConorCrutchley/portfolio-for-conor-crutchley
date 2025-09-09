a9a5db2b6f09c77ca6d5b587388a4d14833dd48a
ConorCrutchley <conorc1993@hotmail.co.uk>
2025-09-09 06:31:31 +0100

    feat: Styled university card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,14 @@
 import type { ReactNode } from 'react';
 import styles from '@/styles/card.module.css';
 
-const Card = ({ children }: { children: ReactNode }) => {
-  return <div className={styles['card']}>{children}</div>;
+const Card = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className: string;
+}) => {
+  return <div className={`${styles['card']} ${className}`}>{children}</div>;
 };
 
 export default Card;
--- src/components/Certificates/University.tsx
+++ src/components/Certificates/University.tsx
@@ -1,20 +1,23 @@
 import Card from '@/components/Card';
 import StaffordshireUniversityLogo from '@/assets/staffordshire-university-square-logo.png';
+import styles from '@/styles/certificates/university.module.css';
 
 const University = () => {
   return (
-    <Card>
-      <img
-        src={StaffordshireUniversityLogo}
-        alt="Staffordshire University Logo"
-      />
-      <div>
-        <h3>
-          BSc Hons
-          <br />
-          Multimedia Computing
-        </h3>
-        <p>First Class Honours</p>
+    <Card className={styles['university-card']}>
+      <div className={styles['university-content']}>
+        <img
+          src={StaffordshireUniversityLogo}
+          alt="Staffordshire University Logo"
+        />
+        <div>
+          <h3>
+            BSc Hons
+            <br />
+            Multimedia Computing
+          </h3>
+          <p>First Class Honours</p>
+        </div>
       </div>
     </Card>
   );
