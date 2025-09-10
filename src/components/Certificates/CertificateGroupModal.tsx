36f54d8fe0d76f09fc031402ecff733f76529a5c
ConorCrutchley <conorc1993@hotmail.co.uk>
2025-09-10 08:30:03 +0100

    feat: Added transitions for modal opening and closing

--- src/components/Certificates/CertificateGroupModal.tsx
+++ src/components/Certificates/CertificateGroupModal.tsx
@@ -10,14 +10,52 @@ const CertificateGroupModal = ({
   certificateGroup: ICertificateGroup;
 }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [btnDisabled, setBtnDisabled] = useState(false);
+  const [classNames, setClassNames] = useState(styles['certificate-modal']);
+
+  const toggleModal = () => {
+    const timeoutDuration = 400;
+    setBtnDisabled(true);
+    const step1ClassName = `${styles['certificate-modal']} ${!isOpen ? styles['certificate-modal-transitioning'] : `${styles['certificate-modal-open']} ${styles['certificate-modal-close-start']}`}`;
+    const step2ClassName = `${styles['certificate-modal']} ${styles['certificate-modal-transitioning']}`;
+    const step3ClassName = `${styles['certificate-modal']} ${!isOpen ? styles['certificate-modal-open'] : ''}`;
+
+    const finalStep = () => {
+      setClassNames(step3ClassName);
+      setIsOpen(!isOpen);
+      setBtnDisabled(false);
+    };
+    setClassNames(step1ClassName);
+    if (isOpen) {
+      setTimeout(() => {
+        setClassNames(step2ClassName);
+        setTimeout(finalStep, timeoutDuration / 4);
+      }, timeoutDuration);
+    } else
+      setTimeout(() => {
+        finalStep();
+      }, timeoutDuration * 2);
+  };
   return (
-    <Card
-      className={`${styles['certificate-modal']} ${isOpen ? styles['certificate-modal-open'] : ''}`}
-    >
+    <Card className={classNames}>
       <div className={styles['certificate-modal-header']}>
-        <h3>{certificateGroup.group}</h3>
+        <h3 className={styles['certificate-modal-header-title']}>
+          {certificateGroup.group}
+        </h3>
         <div className={styles['modal-button-container']}>
-          <button onClick={() => setIsOpen(!isOpen)}>Show Courses</button>
+          <button
+            className={styles['modal-button']}
+            onClick={toggleModal}
+            disabled={btnDisabled}
+            aria-label={
+              !isOpen
+                ? `Show ${certificateGroup.group}`
+                : `Hide ${certificateGroup.group}`
+            }
+          >
+            <span className={styles['modal-button-close']}></span>
+            <span className={styles['modal-button-text']}>Show Courses</span>
+          </button>
         </div>
       </div>
       <div className={styles['certificate-cards']}>
